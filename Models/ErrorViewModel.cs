a9841cbc785530f1c2c490274d836b04f70eef13
Wajeeh Ahmed <wajeehnaeem@yahoo.com>
2018-11-23 12:21:02 +0500

    Created a new dotnet mvc project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GitTestExistingproject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
